667b4ba2ccbf8e5f9ec790699576365ff630ea54
Mathieu Crochet <mcrochet@ineat.fr>
2021-03-25 10:36:37 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 import { SelectField, Spinner, Option } from '@contentful/forma-36-react-components';
@@ -7,94 +6,68 @@ import { init } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
 
-class App extends React.Component {
-  static propTypes = {
-    sdk: PropTypes.object.isRequired,
-  };
-
+function App(props) {
   detachExternalChangeHandler = null;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.sdk.field.getValue(),
-      error: false,
-      hasLoaded: false,
-      items: [],
-    };
-  }
+  const [items, setItems] = useState([]);
+  const [value, setValue] = useState(props.sdk.field.getValue());
+  const [hasLoaded, setHasLoaded] = useState(false);
 
-  componentDidMount() {
-    this.props.sdk.window.startAutoResizer();
+  useEffect(() => {
+    props.sdk.window.startAutoResizer();
+    console.log(props.sdk);
 
-    console.log(this.props.sdk);
-
-    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
+    detachExternalChangeHandler = props.sdk.field.onValueChanged(onExternalChange());
 
     fetch('https://api-staging.mos.com/scholarship-1/ref/virtualform/properties')
       .then((res) => res.json())
       .then(
         (items) => {
-          this.setState({
-            hasLoaded: true,
-            items,
-          });
+          setHasLoaded(true);
+          setItems(items);
         },
         (error) => {
-          this.setState({
-            hasLoaded: true,
-            error: error,
-          });
+          setHasLoaded(true);
         }
       );
-  }
 
-  componentWillUnmount() {
-    if (this.detachExternalChangeHandler) {
-      this.detachExternalChangeHandler();
-    }
-  }
+    return () => {
+      if (detachExternalChangeHandler) {
+        detachExternalChangeHandler();
+      }
+    };
+  }, [props]);
 
   onExternalChange = (value) => {
-    this.setState({ value });
+    setValue(value);
   };
 
   onChange = (e) => {
     const value = e.currentTarget.value;
-    this.setState({ value });
+    setValue(value);
     if (value) {
-      this.props.sdk.field.setValue(value);
+      props.sdk.field.setValue(value);
     } else {
-      this.props.sdk.field.removeValue();
+      props.sdk.field.removeValue();
     }
   };
 
-  render() {
-    if (!this.state.hasLoaded) {
-      return <Spinner />;
-    }
-
-    return (
-      <SelectField
-        id='mos-user-info-input'
-        name='mos-user-info-input'
-        labelText='Select a property'
-        helpText='Link the input to mos-user-info property'
-        value={this.state.value}
-        onChange={this.onChange}
-      >
-        <Option value=''></Option>
-        {this.state.items.map((item) => {
-          return (
-            <Option key={item.name} value={item.name}>
-              {item.name}
-            </Option>
-          );
-        })}
-      </SelectField>
-    );
+  if (!hasLoaded) {
+    return <Spinner />;
   }
+
+  return (
+    <SelectField id='mos-user-info-input' name='mos-user-info-input' labelText='Select a property' helpText='Link the input to mos-user-info property' value={value} onChange={onChange}>
+      <Option value=''></Option>
+      {items.map((item) => {
+        return (
+          <Option key={item.name} value={item.name}>
+            {item.name}
+          </Option>
+        );
+      })}
+    </SelectField>
+  );
 }
 
 init((sdk) => {
